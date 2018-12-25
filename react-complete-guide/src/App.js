acbdd5d636b307e2c2c0e1e68195316d70c45310
Vijay Kumar Patel <vijay.patel@tavant.com>
2018-12-25 21:00:49 +0530

    burger

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,41 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Input from './Input/Input'
-import OutPut from './OutPut/OutPut'
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
- constructor(props){
-   super(props);
-   this.state ={
-    username: 'Aaravi',
-    country: 'India'
-   }
- }
-
- changeIUserName = (event)=>{
-this.setState({
-  username: event.target.value
-});
- }
-  render() {
-    const style = {
-      backgroundColor: 'green'
-    };
+  render () {
     return (
-      <div className="App">
-      <Input
-      textchange={this.changeIUserName}
-      username={this.state.username} 
-      />
-        <OutPut
-          input1='Input text 1 p1'
-          input2='Input text 2 p1'
-          username={this.state.username} />
-
-        <OutPut
-          input1='Input text 1 p2'
-          input2='Input text 2 p2'
-          username={this.state.username}  />
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
